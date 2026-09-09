25b4b7dc1ed01237c5e05eefb5fea6b9cb1a1d20
Martônio Júnior <martoniojunior@hotmail.com>
2026-09-09 19:39:52 -0300

    build: Configure Package.swift
    - Adjust supported platforms
    - Add dependency to CustomDump

--- Package.swift
+++ Package.swift
@@ -1,24 +1,84 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 6.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
+// MARK: - Utilities
+public enum UpcomingFeatures: String, CaseIterable {
+    case existentialAny
+    case fullTypedThrows
+    case internalImportsByDefault
+    case memberImportVisibility
+    case nonescapableTypes
+    case nonisolatedNonsendingByDefault
+    case inferIsolatedConformances
+    case valueGenerics
+    case immutableWeakCaptures
+
+    var asSetting: SwiftSetting { .enableUpcomingFeature(rawValue.prefix(1).uppercased() + rawValue.dropFirst()) }
+}
+
+public extension Array where Element == SwiftSetting {
+    static var upcomingFeatures: Self { UpcomingFeatures.allCases.map(\.asSetting) }
+}
+
+func platformDeps(_ platforms: SupportedPlatform...) -> [SupportedPlatform] {
+    platforms
+}
+
+func targetDep(name: String, package: String, condition: TargetDependencyCondition? = nil) -> Target.Dependency {
+    .product(name: name, package: package, condition: condition)
+}
+
+// MARK: - Traits
+var traits: Set<Trait> = []
+
+traits.insert(
+    .default(enabledTraits: Set(traits.map(\.name)))
+)
+
+// MARK: - Dependencies
+let customDump = targetDep(name: "CustomDump", package: "swift-custom-dump")
+
+let dependencies: [Package.Dependency] = [
+    .package(url: "https://github.com/pointfreeco/swift-custom-dump", .upToNextMajor(from: "1.7.3"))
+]
+
+// MARK: - Targets
+let targets: [Target] = [
+    .target(
+        name: "Evaluation",
+        dependencies: [customDump],
+        swiftSettings: .upcomingFeatures
+    )
+]
+
+let testTargets: [Target] = targets.map { t in
+    .testTarget(name: "\(t.name)Tests", dependencies: [Target.Dependency(stringLiteral: t.name)] + t.dependencies)
+}
+
+// MARK: - Products
+let products: [Product] = [
+    .library(
+        name: "Evaluation",
+        targets: ["Evaluation"]
+    )
+]
+
+// MARK: - Supported Platforms
+let platforms: [SupportedPlatform] = [
+    .macOS(.v10_15),
+    .iOS(.v13),
+    .watchOS(.v5),
+    .tvOS(.v13)
+]
+
+// MARK: - PackageDescription
 let package = Package(
     name: "Evaluation",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Evaluation",
-            targets: ["Evaluation"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Evaluation"),
-        .testTarget(
-            name: "EvaluationTests",
-            dependencies: ["Evaluation"]
-        ),
-    ]
+    platforms: platforms,
+    products: products,
+    dependencies: dependencies,
+    targets: targets + testTargets,
+    swiftLanguageModes: [.v6]
 )
